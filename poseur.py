0d3ab7aa7d0885752ece017db9d7170cda1a3d7a
Christian Stigen Larsen <csl@sublevel3.org>
2009-12-28 00:13:15 +0100

    Changed to pyglet

--- poseur.py
+++ poseur.py
@@ -1,44 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-from PyQt4 import QtGui, QtCore, QtOpenGL
+import pyglet
+from pyglet.gl import *
 
-#try:
-#	from OpenGL import GL
-#except ImportError:
-#	app = QtGui.QApplication(sys.argv)
-#	print("PyOpenGL must be installed")
-	#sys.exit(1)
-
-def init():
-	"Set up graphical environment"
-	pass
-
-def read(fp):
-	"Read presentation from file"
-	return {'slide': {'text': 'Hello'}}
-
-def log(str):
-	print(str)
-
-def show(pres):
-	"Show presentation"
-	pass
-
-class Wiggy(QtOpenGL.QGLWidget):
-	def __init__(self, parent):
-		QtOpenGL.QGLWidget.__init__(self, parent)
-		self.setMinimumSize(500, 500)
-
-class Test(QtGui.QMainWindow):
+class Slideshow(pyglet.window.Window):
 	def __init__(self):
-		QtGui.QMainWindow.__init__(self)
-		w = Wiggy(self)
-		self.setCentralWidget(w)
+		pyglet.window.Window.__init__(self,
+			caption='Poseur',
+			visible=False,
+			vsync=True,
+			fullscreen=False)
+
+		self.label = pyglet.text.Label('Hello!',
+			font_name='Times New Roman',
+			font_size=36,
+			x=self.width//2,
+			y=self.height//2,
+			anchor_x='center',
+			anchor_y='center')
+
+		# allow time to init window before displaying,
+		# to avoid annoying white-out before going black
+
+		self.set_visible()
+		self.x = 320
+		self.y = 240
+		self.rot = 0.4
+		self.size = 300.0
+
+	def on_draw(self):
+		self.clear()
+		glPushMatrix()
+
+		glLoadIdentity()
+		glTranslatef(self.x, self.y, 0.0)
+		glRotatef(self.rot, 0, 0, 1)
+		glScalef(self.size, self.size, 1.0)
+
+		glBegin(GL_TRIANGLES)
+		glColor4f(1.0, 0.0, 0.0, 0.0)
+		glVertex2f(0.0, 0.5)
+		glColor4f(0.0, 0.0, 1.0, 1.0)
+		glVertex2f(0.2, -0.5)
+		glColor4f(0.0, 0.0, 1.0, 1.0)
+		glVertex2f(-0.2, -0.5)
+		glEnd()
+
+		glPopMatrix()
+		self.rot = self.rot + 1.1
+		self.label.draw()
 
 if __name__ == "__main__":
-	a = QtGui.QApplication(sys.argv)
-	w = Test()
-	w.show()
-	a.exec_()
+	window = Slideshow()
+	pyglet.app.run()
